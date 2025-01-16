3392e2a5c123b56a2072677b88f6c9d5322b60ea
Qaid-Danial <muhammadqaidd@gmail.com>
2025-01-17 00:05:59 +0800

    commit 3

--- src/main.cpp
+++ src/main.cpp
@@ -6,4 +6,26 @@ using namespace std;
 
 int main()
 {
+    ifstream infile("test.txt");
+    Game g(infile);
+    vector<Team> teams;
+
+    for (int i = 0; i < 2; i++)
+    {
+        teams.push_back(Team(infile));
+    }
+
+    g.print();
+    for (int i = 0; i < 2; i++)
+    {
+        teams[i].print();
+    }
+
+    cout << endl;
+    cout << endl;
+
+    tuple<string, char, int> ship1 = teams[0].getShip(1);
+    cout << "Type = " << get<0>(ship1) << ", "
+         << "Symbol = " << get<1>(ship1) << ", "
+         << "Amount = " << get<2>(ship1) << endl;
 }
